002d38bbd695c4aec14d49da17ff1c07298614f1
pancho horrillo <pancho@pancho.name>
2025-07-16 22:03:21 +0200

    feat: enable Elpaca
    See:
    - https://github.com/progfolio/elpaca/blob/master/doc/early-init.el
    - https://github.com/progfolio/elpaca/blob/master/doc/init.el

---
+++ lisp/early-elpaca.el
@@ -0,0 +1,13 @@
+;;; early-elpaca.el --- Early Elpaca installer -*- lexical-binding: t; -*-
+
+;; https://github.com/progfolio/elpaca/blob/master/doc/early-init.el
+(setq package-enable-at-startup nil)
+
+(provide 'early-elpaca)
+;;; early-elpaca.el ends here
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
