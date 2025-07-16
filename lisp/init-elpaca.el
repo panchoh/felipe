7071c9b597cffa371cb67cf720b19b4301fc27d8
pancho horrillo <pancho@pancho.name>
2025-07-16 22:07:51 +0200

    feat(elpaca-installer): handle multiple Emacs vers
    https://github.com/progfolio/elpaca/wiki/Using-multiple-Emacs-versions

--- lisp/init-elpaca.el
+++ lisp/init-elpaca.el
@@ -1,7 +1,7 @@
 ;; init-elpaca.el --- Elpaca installer -*- lexical-binding: t; -*-
 (defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-builds-directory (expand-file-name (concat "builds-" emacs-version) elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                        :ref nil :depth 1 :inherit ignore
