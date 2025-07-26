196ee6c8c67ed7aea35198e384e11fce2903b9c3
pancho horrillo <pancho@pancho.name>
2025-07-26 08:49:25 +0200

    feat(init-emacs): enable smooth scrolling

--- lisp/init-emacs.el
+++ lisp/init-emacs.el
@@ -8,7 +8,9 @@
   (setq auto-save-visited-interval 0)
   (auto-save-visited-mode 1)
   (setq make-backup-files nil)
-  (setq visible-bell t))
+  (setq visible-bell t)
+  (setq pixel-scroll-precision-large-scroll-height 40.0)
+  (pixel-scroll-precision-mode))
 
 (provide 'init-emacs)
 ;;; init-emacs.el ends here
